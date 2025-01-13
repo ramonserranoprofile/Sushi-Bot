85fe6bbc3b5d934c4527b149a3dfd8a1cf816ef4
Ramon Serrano <ramonserrano76@gmail.com>
2025-01-13 13:33:53 -0300

    commit drag & drop image, enriched text box

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'semantic-ui-css/semantic.min.css'
+import 'react-quill/dist/quill.snow.css';
+
 // modern structure for main.jsx
 const root = createRoot(document.getElementById('root'))
 root.render(
